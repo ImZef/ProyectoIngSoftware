f1a6e58dd446cca9e7835bdbad3e8341e8ae0e48
ImZef <92174315+ImZef@users.noreply.github.com>
2025-06-20 19:14:10 -0500

    Archivos main.py y Producto.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from Producto import Producto
+
+producto1 = Producto(1, "Leche", "Lácteos", "Leche entera 1L", 4000, 20, "19/08/2025")
+producto2 = Producto(2, "Pan", "Panadería", "Pan integral", 12000, 0, "22/06/2025")
+producto3 = Producto(3, "Atún", "Conservas", "Lata de atún en agua", 10000, 15, "01/01/2026")
+producto2 = Producto(4, "Pan artesano", "Panadería", "Pan artesanal", 15000, 3, "02/07/2025")
+
+#Producto.ver_productos()
+#Producto.consultar_codigo(2)
+#Producto.consultar_nombre("Leche")
+#Producto.consultar_nombre("Pan")
+#Producto.consultar_categoria("Panadería")
+Producto.consultar_codigo(5)
